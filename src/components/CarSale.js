7f77c0697f9ec78f4621e90717c76b30fb7d2b3f
Wad Werton <wad_werton@mail.ru>
2020-05-10 22:05:52 +0300

    feat: get request cars and dictinoray

--- src/components/CarSale.js
+++ src/components/CarSale.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
 
 import './CarSale.css';
 import SelectionCar from './SelectionCar';
 import CarList from './CarList';
+import { getCars, getDict } from '../redux/actions';
 
-const CarSale = () => {
+const CarSale = (props) => {
+    const { cars, isListLoaded, dict, isDictLoaded, getCars, getDict } = props;
+    
+    useEffect(() => {
+        if (!isListLoaded) {
+           getCars(); 
+        } 
+        if (!isDictLoaded) {
+            getDict(); 
+         } 
+    });
 
     return (
         <div className="wrapper">
@@ -14,4 +26,15 @@ const CarSale = () => {
     )
 }
 
-export default CarSale;
+const mapStateToProps = (state) => {
+    return {
+      cars: state.carSale.list,
+      isListLoaded: state.carSale.isListLoaded,
+      dict: state.carSale.dict,
+      isDictLoaded: state.carSale.isDictLoaded,
+    };
+  }
+
+  
+
+  export default connect(mapStateToProps , { getCars, getDict })(CarSale);
