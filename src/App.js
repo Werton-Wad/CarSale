106f85b8e3f92636bb9f3e45121b124735f8fa2c
Wad Werton <wad_werton@mail.ru>
2020-05-10 20:22:49 +0300

    init: start CarSale app

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import CarSale from './components/CarSale';
+
+
+function App() {
+  return (
+    <>
+        <CarSale />
+    </>
+  );
+}
+
+export default App;
+
+
---
+++ src/components/CarSale.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CarSale = () => {
+
+    return (
+        <div>
+            CarSale
+        </div>
+    )
+}
+
+export default CarSale;
