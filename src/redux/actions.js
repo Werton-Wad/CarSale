4918a2847c7bc86b5b24ac3b066dfaeef6f488bf
Wad Werton <wad_werton@mail.ru>
2020-05-10 21:01:57 +0300

    feat: add redux/actions getDict

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -4,16 +4,27 @@ import config from 'config';
 
 export const actions = {
   getCars: 'GET_CARS',
+  getDict: 'GET_DICTINARY'
 };
 
 
 export const getCars = () => {
-  return async (dispatch) => {
-    try {
-      const { data: cars } = await axios(config.apiCars)
-      dispatch({ type: actions.getCars, payload: cars});
-    } catch (error) {
-      console.log(error);
+    return async (dispatch) => {
+        try {
+            const { data: cars } = await axios(config.apiCars)
+            dispatch({ type: actions.getCars, payload: cars });
+        } catch (error) {
+            console.log(error);
+        }
     }
-  }
+}
+export const getDict= () => {
+    return async (dispatch) => {
+        try {
+            const { data: dict } = await axios(config.apiDict)
+            dispatch({ type: actions.getDict, payload: dict });
+        } catch (error) {
+            console.log(error);
+        }
+    }
 }
